2fb9ae82a38a39c7f364a38878766198e2ec679e
Rafael <rafafd@hotmail.com>
2020-05-01 11:25:55 -0300

    create NurseDAO and NurseController

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ use Slim\Factory\AppFactory;
 use Slim\Routing\RouteCollectorProxy;
 
 require_once "Controller/VaccineController.php";
+require_once "Controller/NurseController.php";
 
 require __DIR__ . '/vendor/autoload.php';
 
@@ -18,6 +19,15 @@ $app->group('/api/vacinas', function (RouteCollectorProxy $group) {
     $group->get('/{id:[0-9]+}', 'VaccineController:listById');
     $group->put('/{id:[0-9]+}', 'VaccineController:update');
     $group->delete('/{id:[0-9]+}', 'VaccineController:delete');
+});
+
+$app->group('/api/enfermeiros', function (RouteCollectorProxy $group) {
+    $group->post('[/]', 'NurseController:insert');
+    $group->get('[/]', 'NurseController:list');  
+    $group->get('/{id:[0-9]+}', 'NurseController:listById');
+    $group->get('/coren/{coren:[0-9]+}', 'NurseController:listByCoren');
+    $group->put('/{id:[0-9]+}', 'NurseController:update');
+    $group->delete('/{id:[0-9]+}', 'NurseController:delete');
 });
 
 
